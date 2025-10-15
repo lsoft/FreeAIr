eec5fd383931db5d7a785063cb3dafcecac14aec
lsoft <lsoft@list.ru>
2025-10-15 22:40:42 +0500

    refs #49 Added additional logging.

--- FreeAIr/Find/FindWindowModifier.cs
+++ FreeAIr/Find/FindWindowModifier.cs
@@ -1,9 +1,12 @@
 ﻿using EnvDTE80;
 using EnvDTE90;
 using FreeAIr.Helper;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Drawing.Printing;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,62 +28,113 @@ namespace FreeAIr.Find
 
                 while (true)
                 {
-                    foreach (System.Windows.Window w in Application.Current.Windows)
+                    var telemetry = new FindAllTelemetryS();
+                    telemetry.AddStep("start");
+
+                    try
                     {
-                        var findFilesDialogControl = w.GetRecursiveByTypeOrName("FindFilesDialogControl", null);
-                        if (findFilesDialogControl == null)
+                        foreach (System.Windows.Window w in Application.Current.Windows)
                         {
-                            continue;
+                            telemetry.AddStep("first line in foreach");
+
+                            var findFilesDialogControl = w.GetRecursiveByTypeOrName("FindFilesDialogControl", null);
+                            if (findFilesDialogControl == null)
+                            {
+                                continue;
+                            }
+
+                            telemetry.AddStep("after continue FindFilesDialogControl");
+
+                            var findAllButton = findFilesDialogControl.GetRecursiveByName("FindAll") as Button;
+                            if (findAllButton == null)
+                            {
+                                continue;
+                            }
+
+                            telemetry.AddStep("after continue FindAll");
+
+                            var textBoxes = new List<TextBox>();
+                            findFilesDialogControl.GetRecursiveByType(ref textBoxes);
+
+                            telemetry.AddStep($"after searching textboxes, found: {textBoxes.Count}");
+                            textBoxes.ForEach(tb => telemetry.AddTextBox(tb));
+
+                            var checkBoxes = new List<CheckBox>();
+                            findFilesDialogControl.GetRecursiveByType(ref checkBoxes);
+
+                            telemetry.AddStep($"after searching checkBoxes, found: {checkBoxes.Count}");
+                            checkBoxes.ForEach(cb => telemetry.AddCheckBox(cb));
+
+                            var sortedTextBoxes = (
+                                from textBox in textBoxes
+                                where textBox.Visibility == Visibility.Visible && textBox.ActualHeight >= double.Epsilon
+                                let crd = textBox.PointToScreen(new Point(0, 0))
+                                orderby crd.Y
+                                select textBox
+                                ).ToList();
+
+                            telemetry.AddStep($"after sorting textBoxes, found: {sortedTextBoxes.Count}");
+                            sortedTextBoxes.ForEach(tb => telemetry.AddSortedTextBox(tb));
+
+                            var subjectToSearchTextBox = sortedTextBoxes[0];
+                            var fileTypesFilterTextBox = sortedTextBoxes[2];
+
+                            var ragCheckBox = CreateUseRAGCheckBox(
+                                checkBoxes.FirstOrDefault(),
+                                findAllButton,
+                                subjectToSearchTextBox,
+                                fileTypesFilterTextBox
+                                );
+
+                            telemetry.AddStep($"after ragCheckBox");
+
+                            var naturalSearchButton = CreateNaturalLanguageSearchButton(
+                                ragCheckBox,
+                                findAllButton,
+                                subjectToSearchTextBox,
+                                fileTypesFilterTextBox
+                                );
+
+                            telemetry.AddStep($"after naturalSearchButton");
+
+                            //put the new button
+                            var parent = VisualTreeHelper.GetParent(findAllButton) as WrapPanel;
+
+                            if (parent is not null)
+                                telemetry.AddStep($"after find parent, parent is NOT null");
+                            else
+                                telemetry.AddStep($"after find parent, parent is null");
+
+                            parent.Children.Insert(
+                                0,
+                                naturalSearchButton
+                                );
+
+                            telemetry.AddStep($"after inserting naturalSearchButton");
+
+                            parent.Children.Insert(
+                                0,
+                                ragCheckBox
+                                );
+
+                            telemetry.AddStep($"SUCCESS");
+
+                            return;
                         }
-
-                        var findAllButton = findFilesDialogControl.GetRecursiveByName("FindAll") as Button;
-                        if (findAllButton == null)
-                        {
-                            continue;
-                        }
-
-                        var textBoxes = new List<TextBox>();
-                        findFilesDialogControl.GetRecursiveByType(ref textBoxes);
-
-                        var checkBoxes = new List<CheckBox>();
-                        findFilesDialogControl.GetRecursiveByType(ref checkBoxes);
-
-                        var sortedTextBoxes = (
-                            from textBox in textBoxes
-                            where textBox.Visibility == Visibility.Visible && textBox.ActualHeight >= double.Epsilon
-                            let crd = textBox.PointToScreen(new Point(0, 0))
-                            orderby crd.Y
-                            select textBox
-                            ).ToList();
-                        var subjectToSearchTextBox = sortedTextBoxes[0];
-                        var fileTypesFilterTextBox = sortedTextBoxes[2];
-
-                        var ragCheckBox = CreateUseRAGCheckBox(
-                            checkBoxes.FirstOrDefault(),
-                            findAllButton,
-                            subjectToSearchTextBox,
-                            fileTypesFilterTextBox
+                    }
+                    catch (Exception excp)
+                    {
+                        telemetry.AddStep($"FAIL");
+                        telemetry.AddStep(excp.Message);
+                        telemetry.AddStep(excp.StackTrace);
+                    }
+                    finally
+                    {
+                        var serialized = JsonConvert.SerializeObject(telemetry, Formatting.Indented);
+                        ActivityLog.LogError(
+                            "FreeAIr (Find Window)",
+                            serialized
                             );
-                        var naturalSearchButton = CreateNaturalLanguageSearchButton(
-                            ragCheckBox,
-                            findAllButton,
-                            subjectToSearchTextBox,
-                            fileTypesFilterTextBox
-                            );
-
-                        //put the new button
-                        var parent = VisualTreeHelper.GetParent(findAllButton) as WrapPanel;
-                        parent.Children.Insert(
-                            0,
-                            naturalSearchButton
-                            );
-                        parent.Children.Insert(
-                            0,
-                            ragCheckBox
-                            );
-
-
-                        return;
                     }
 
                     await Task.Delay(250, ct);
@@ -177,6 +231,166 @@ namespace FreeAIr.Find
 
             return naturalSearchButton;
 
+        }
+    }
+
+    public class FindAllTelemetryS
+    {
+        public List<string> Steps
+        {
+            get;
+            set;
+        }
+        public void AddStep(string step)
+        {
+            Steps.Add(step);
+        }
+
+        public List<TextBoxS> TextBoxes
+        {
+            get;
+            set;
+        }
+
+        public List<CheckBoxS> CheckBoxes
+        {
+            get;
+            set;
+        }
+
+        public List<TextBoxS> SortedTextBoxes
+        {
+            get;
+            set;
+        }
+
+        public void AddTextBox(TextBox textBox)
+        {
+            var position = textBox.PointToScreen(new Point(0, 0));
+
+            TextBoxes.Add(
+                new TextBoxS
+                {
+                    Name = textBox?.Name ?? "NULL",
+                    Content = textBox?.Text ?? "NULL",
+                    X = position.X,
+                    Y = position.Y,
+                    IsEnabled = textBox.IsEnabled
+                }
+                );
+        }
+
+        public void AddSortedTextBox(TextBox textBox)
+        {
+            var position = textBox.PointToScreen(new Point(0, 0));
+
+            SortedTextBoxes.Add(
+                new TextBoxS
+                {
+                    Name = textBox?.Name ?? "NULL",
+                    Content = textBox?.Text ?? "NULL",
+                    X = position.X,
+                    Y = position.Y,
+                    IsEnabled = textBox.IsEnabled
+                }
+                );
+        }
+
+        public void AddCheckBox(CheckBox checkBox)
+        {
+            var position = checkBox.PointToScreen(new Point(0, 0));
+
+            CheckBoxes.Add(
+                new CheckBoxS
+                {
+                    Name = checkBox?.Name ?? "NULL",
+                    Content = checkBox.Content?.ToString() ?? "NULL",
+                    X = position.X,
+                    Y = position.Y,
+                    IsEnabled = checkBox.IsEnabled,
+                    IsChecked = checkBox.IsChecked
+                }
+                );
+        }
+
+        public FindAllTelemetryS()
+        {
+            Steps = new();
+            TextBoxes = new();
+            CheckBoxes = new();
+            SortedTextBoxes = new();
+        }
+    }
+
+    public class TextBoxS
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Content
+        {
+            get;
+            set;
+        }
+
+        public double X
+        {
+            get;
+            set;
+        }
+
+        public double Y
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled
+        {
+            get;
+            set;
+        }
+    }
+
+    public class CheckBoxS
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Content
+        {
+            get;
+            set;
+        }
+
+        public bool? IsChecked
+        {
+            get;
+            set;
+        }
+
+        public double X
+        {
+            get;
+            set;
+        }
+
+        public double Y
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled
+        {
+            get;
+            set;
         }
     }
 
